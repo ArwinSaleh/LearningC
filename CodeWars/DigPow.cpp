b3406a8bc0f2a373bdaacb8e4d9b093c40728cb3
Arwin Saleh <arwinsaleh@hotmail.com>
2021-01-08 19:15:07 +0100

    commit

--- CodeWars/DigPow.cpp
+++ CodeWars/DigPow.cpp
@@ -1,31 +1,47 @@
-class DigPow
-{
-public:
-  static int digPow(int n, int p){
-    int sum = 0;
-    int k = 1;
-    int nTmp = n;
-    
-    while (nTmp > 0){
-      sum += pow( (nTmp % 10), p );
-      p += 1;
-      nTmp /= 10;
+#include <iostream>
+using namespace std;
+
+int reverseNumber(int n)  
+{  
+int reverse=0, rem;    
+  while(n!=0)    
+  {    
+     rem=n%10;      
+     reverse=reverse*10+rem;    
+     n/=10;    
+  }    
+return reverse;  
+}  
+
+static int pow(int n, int p){
+  int res = n;
+  for (int i = 0; i < p; i++){
+    res *= res;
+  }
+  return res;
+}
+
+static int digPow(int n, int p){
+  int sum = 0;
+  int k = 1;
+  int nTmp = n;
+  
+  while (nTmp > 0){
+    sum += pow( (reverseNumber(nTmp) % 10), p );
+    p += 1;
+    nTmp /= 10;
+  }
+  while ( (k * n) < sum){
+    if ( (k * n) == sum ){
+      return k;
     }
-    while ( (k * n) < sum){
-      if ( (k * n) == sum ){
-        return k;
-      }
-      k += 1;
-    }
-    
-    return -1;
+    k += 1;
   }
   
-  static int pow(int n, int p){
-    int res = n;
-    for (int i = 0; i < p; i++){
-      res *= res;
-    }
-    return res;
-  }
-};
+  return -1;
+}
+
+int main(){
+  cout << digPow(89, 1);
+  return 0;
+}
